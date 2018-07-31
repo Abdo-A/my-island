580799604707fa68c3efb5011a1fbcf0589aa363
Abdo-A <abdopoetry@gmail.com>
2018-07-31 09:17:58 +0200

    test

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 import "./Events.css";
 
-const RapidAPI = require("rapidapi-connect");
-const rapid = new RapidAPI(
-  "loloworld_5b0f0f20e4b089f3dc475407",
-  "170de709-c804-4a9e-818f-ecd3fde83992"
-);
-
 export default class Events extends Component {
   state = {
     events: null,
